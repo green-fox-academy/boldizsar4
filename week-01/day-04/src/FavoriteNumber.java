5c26058ac8e18ed5d2ca16a80c7f39c99d02c200
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-10 20:03:25 +0200

    structure fix

---
+++ week-01/day-04/src/FavoriteNumber.java
@@ -0,0 +1,9 @@
+public class FavoriteNumber {
+    public FavoriteNumber() {
+    }
+
+    public static void main(String[] args) {
+        int favoriteNumber = 4;
+        System.out.println("My favorite number is: " + favoriteNumber);
+    }
+}
