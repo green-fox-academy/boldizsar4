df6230d587d4aa97334a914a040d17145abd4d93
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-10 19:58:06 +0200

    structure fix

---
+++ week-01/day-04/src/TwoNumbers.java
@@ -0,0 +1,20 @@
+public class TwoNumbers {
+    public TwoNumbers() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(35);
+        System.out.println(-9);
+        System.out.println(286);
+        System.out.println(1.6923076923076923D);
+        Integer numberDivide = 22;
+        System.out.println(numberDivide / 13);
+
+        int i = 22;
+        int j = 13;
+        System.out.println("i is " + i);
+        System.out.println("j is " + j);
+        int k = i / j;
+        System.out.println("i/j is " + k);
+    }
+}
