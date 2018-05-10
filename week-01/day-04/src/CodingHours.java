1fd5d6d8ffb323405362368600131050befbffb6
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-10 20:02:27 +0200

    structure fix

---
+++ week-01/day-04/src/CodingHours.java
@@ -0,0 +1,17 @@
+public class CodingHours {
+    public CodingHours() {
+    }
+
+    public static void main(String[] args) {
+        int codeDailyHours = 6;
+        int semesterDays = 85;
+        int k = codeDailyHours * semesterDays;
+        System.out.println(k + " hours spent an attendee with coding in a semester.");
+        int codingHoursSemester = 510;
+
+        int averageHoursWeekly = 52;
+        double averageHoursSemester = 884;
+        double percentageOfCoding = (double)codingHoursSemester / averageHoursSemester;
+        System.out.println("percentage of coding hours is " + percentageOfCoding * 100.0D);
+    }
+}
