a07630eeebae84996ef66f4fc7c03ac5c230e1b1
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-10 19:56:38 +0200

    structure fix

---
+++ week-01/day-04/src/IntroduceYourself.java
@@ -0,0 +1,10 @@
+public class IntroduceYourself {
+    public IntroduceYourself() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Name - Boldizsár");
+        System.out.println("Age - 22");
+        System.out.println("Address - Budapest");
+    }
+}
