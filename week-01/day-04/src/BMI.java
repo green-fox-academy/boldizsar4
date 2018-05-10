4b9c23634cf970ef0dc19759141149ea662b3ee3
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-10 20:06:11 +0200

    structure fix

---
+++ week-01/day-04/src/BMI.java
@@ -0,0 +1,11 @@
+public class BMI {
+    public BMI() {
+    }
+
+    public static void main(String[] args) {
+        double massInKg = 81.2D;
+        double heightInM = 1.78D;
+        double bmi = 25.628077262971846D;
+        System.out.println("Your BMI is : " + bmi);
+    }
+}
