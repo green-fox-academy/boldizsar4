13166267ffe1db87d4b16839702275adf1c5e00b
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-10 20:07:03 +0200

    structure fix

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,20 @@
+public class DefineBasicInfo {
+    public DefineBasicInfo() {
+    }
+
+    public static void main(String[] args) {
+        String name = "Boldizsár";
+        System.out.println(name);
+        Integer age = 22;
+        System.out.println(age);
+        Double height = 1.61D;
+        System.out.println(height);
+        boolean notMarried = true;
+        if (notMarried) {
+            System.out.println("Not Married");
+        } else {
+            System.out.println("Married");
+        }
+
+    }
+}
