2f7c4ff6542eb5f234c9c94eed8b705e288b5e99
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-14 14:13:00 +0200

    greeting

---
+++ week-02/day-01/src/Greet.java
@@ -0,0 +1,15 @@
+public class Greet {
+    public static void main(String[] args) {
+
+        String alx = "Greenfox";
+        
+        greet(alx);
+    }
+
+        public static void greet (String al) {
+            System.out.println("Greetings dear, " + al);
+
+        }
+
+    }
+
