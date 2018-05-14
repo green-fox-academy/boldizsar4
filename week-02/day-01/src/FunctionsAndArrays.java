868e1e01c43c303869b772e79a03c778e42fdd31
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-14 10:29:46 +0200

    functions and arrays

---
+++ week-02/day-01/src/FunctionsAndArrays.java
@@ -0,0 +1,18 @@
+public class FunctionsAndArrays {
+    public static void main(String[] args) {
+        int [] numbers = new int [4];
+        String [] texts = new String [4];
+        boolean [] truth = new boolean[4];
+
+        numbers [0] = 1;
+        numbers [1] = 5;
+        numbers [2] = 8;
+        numbers [3] = 4;
+
+        int [] numbers2 = {3, 6, 9, 2};
+        int [] numbers3;
+
+        numbers3 = new int[] {9, 8, 7, 6, 4};
+        System.out.println(numbers2);
+    }
+}
