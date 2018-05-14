1bb6fad2869f1bbc5a2b1ae1b867f4339bba3d09
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-14 22:57:33 +0200

    reverse

---
+++ week-02/day-01/src/Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int[] aj = {3, 4, 5, 6, 7};
+
+        for (int i = 4; i >= 0; i--) {
+            System.out.println(aj[i]);
+        }
+    }
+}
