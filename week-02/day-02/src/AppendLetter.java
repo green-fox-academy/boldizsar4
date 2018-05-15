2f7e8825b685865e261d011d16a91a07014291ad
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-15 15:22:03 +0200

    append letter

---
+++ week-02/day-02/src/AppendLetter.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AppendLetter {
+    public static void main(String[] args) {
+        List<String> far = Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk");
+        System.out.println(appendA(far));
+    }
+    public static List<String> appendA(List<String > far){
+        String a = "a";
+        for (int i = 0; i < far.size(); i++) {
+        far.set(i, far.get(i) + a);
+
+        }
+        return far;
+    }
+}
+
+
+// Create a method called "appendA()" that adds "a" to every string in the "far" list.
+// The parameter should be a list.
+// The output should be: "kutya", "macska", "kacsa", "róka", "halacska"
