406acca9822844a8f5f2b541dd82d6676b881f89
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-17 19:44:24 +0200

    rainbow function

--- week-02/day-04/src/RainbowBoxFunction.java
+++ week-02/day-04/src/RainbowBoxFunction.java
@@ -6,19 +6,27 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RainbowBoxFunction {
     public static void mainDraw(Graphics graphics) {
+
         // Create a square drawing function that takes 2 parameters:
         // The square size, and the fill color,
         // and draws a square of that size and color to the center of the canvas.
         // Create a loop that fills the canvas with rainbow colored squares.
-int size = (int) (Math.random()*133);
-int green = 200;
-rainbow(graphics,size,green);
+
+        int size = WIDTH;
+        int color = (int) (Math.random()*256);
+        for (int i = 0; i < 10; i++) {
+            rainbow(graphics,size,color);
+        }
     }
-    public static void rainbow(Graphics graphics,int size,int green){
-        for (int i = 0; i < 4; i++) {
-            Random random = new Random();
-            graphics.setColor(new Color((int)(Math.random()*128.5)*i,green,(int)(Math.random()*128.5)*i,100));
-            graphics.fillRect((WIDTH/2) - (size/2),(HEIGHT/2)-(size/2),size*i,size*i);
+
+public static void rainbow(Graphics graphics,int square,int moreColor) {
+
+        for (int i = 0; i < 10; i++) {
+        square -= 30;
+        int x = (WIDTH/2)-(square/2);
+        int y = (HEIGHT/2)-(square/2);
+        graphics.setColor(new Color(moreColor,(int)(Math.random()*256),(int)(Math.random()*256),100));
+        graphics.fillRect(x,y,square,square);
         }
     }
 
