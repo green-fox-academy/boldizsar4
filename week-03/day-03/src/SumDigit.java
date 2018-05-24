cd67a0d553718a0fc03599b8dddf843d270a77d0
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-24 11:54:54 +0200

    sum of digits

---
+++ week-03/day-03/src/SumDigit.java
@@ -0,0 +1,19 @@
+public class SumDigit {
+    public static void main(String[] args) {
+
+        int n = 1995;
+        int sum = sumDigit(n);
+        System.out.println(sum);
+    }
+
+    private static int sumDigit(int n) {
+        if (n == 0) {
+            return 0;
+        } else
+            return (n % 10) + sumDigit(n / 10);
+    }
+}
+
+// Given a non-negative int n, return the sum of its digits recursively (no loops).
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
