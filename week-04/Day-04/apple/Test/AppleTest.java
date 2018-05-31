62b666d25964126791a2331748b51e0da293cb25
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-31 13:23:59 +0200

    apple test done

---
+++ week-04/Day-04/apple/Test/AppleTest.java
@@ -0,0 +1,22 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleTest {
+
+  static Apple getApple;
+
+  @BeforeClass
+  public static void init(){
+  getApple = new Apple();
+}
+@Test
+  public void should_return_apple_with_getApple(){
+
+  String expectedResult = "apple";
+  String result = getApple.getApple();
+
+  assertEquals(expectedResult,result);
+}
+}
