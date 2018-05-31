31692002c9eb70ce44babee03ea92510442f430e
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-31 13:10:31 +0200

    sumTest done

--- week-04/Day-04/sum/Test/SumTest.java
+++ week-04/Day-04/sum/Test/SumTest.java
@@ -21,9 +21,29 @@ public class SumTest {
   }
 
   @Test
-  public void should_return_with_oneIntegerInput(){
+  public void should_returnNumber_when_oneIntegerInput(){
+
     int inputNumber = 10;
     int expectedResult = 10;
     int result = sum.getSum(inputNumber);
+
+    assertEquals(expectedResult,result);
+  }
+
+  @Test
+  public void should_returnSum_multipleIntegerInput(){
+
+    int[] numbers = new int[] {1,2,3};
+    int expectedResult = 6;
+    int result = sum.getSum(numbers);
+
+    assertEquals(expectedResult,result);
+  }
+  @Test
+  public void should_return0_when_nullInput(){
+    int expectedResult = 0;
+    int result = sum.getSum(null);
+
+    assertEquals(expectedResult,result);
   }
 }
--- week-04/Day-04/sum/src/Sum.java
+++ week-04/Day-04/sum/src/Sum.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Sum {
 
   public int getSum(int... numbers){
+    if (numbers == null){
+      return 0;
+    }
     int sum = 0;
     for (int number : numbers){
     sum += number;
