85798c86570337a6ea4400b30d2ae8cc0f80c3ed
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-05-31 14:20:18 +0200

    feat(anagramtest):create test methods

--- week-04/Day-04/anagram/Test/AnagramTest.java
+++ week-04/Day-04/anagram/Test/AnagramTest.java
@@ -1,5 +1,26 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class AnagramTest {
+  static Anagram isAnagram;
 
+  @BeforeClass
+  public static void init(){
+    isAnagram = new Anagram();
+  }
+
+  @Test
+  public void should_returnFalse_when_stringLengthNotEquals(){
+    String s1 = "abc";
+    String s2 = "kjld";
+    Assert.assertFalse(isAnagram.isAnagram(s1, s2));
+  }
+
+  @Test
+  public void should_returnTrue_when_stringIsAnagram(){
+    String s1 = "Tar";
+    String s2 = "rat";
+    Assert.assertTrue(isAnagram.isAnagram(s1,s2));
+  }
 }
