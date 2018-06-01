172d3dc70bdeebfc37667f004c17ac619b6b8380
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-06-01 13:03:52 +0200

    feat(test):create new test function

--- week-04/Day-04/anagram/Test/AnagramTest.java
+++ week-04/Day-04/anagram/Test/AnagramTest.java
@@ -1,26 +1,44 @@
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class AnagramTest {
-  static Anagram isAnagram;
+  static Anagram anagram;
 
   @BeforeClass
   public static void init(){
-    isAnagram = new Anagram();
+    anagram = new Anagram();
   }
 
   @Test
   public void should_returnFalse_when_stringLengthNotEquals(){
-    String s1 = "abc";
-    String s2 = "kjld";
-    Assert.assertFalse(isAnagram.isAnagram(s1, s2));
+    String s1 = "sds";
+    String s2 = "abss";
+    assertFalse(anagram.isAnagram(s1, s2));
   }
 
   @Test
   public void should_returnTrue_when_stringIsAnagram(){
     String s1 = "Tar";
     String s2 = "rat";
-    Assert.assertTrue(isAnagram.isAnagram(s1,s2));
+    Assert.assertTrue(anagram.isAnagram(s1,s2));
   }
-}
+
+  @Test
+    public void should_returnFalse_when_its_notAnagram() {
+    String s1 = "abcc";
+    String s2 = "abab";
+    assertFalse(anagram.isAnagram(s1, s2));
+  }
+
+  @Test
+  public void sévjspdfks(){
+    String s1 = "";
+    String s2 = "";
+    assertFalse(anagram.isAnagram(s1,s2));
+  }
+  }
+
