d68dbe6e98490fd3ddf95130316fd2ce1b33820b
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-06-25 13:36:00 +0200

    hello user rest app done

--- week-07/day-01/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloRESTController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloRESTController.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.helloworld.controllers;
 
 import com.greenfoxacademy.helloworld.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,8 @@ public class HelloRESTController {
 Greeting greeting;
 
 @RequestMapping(value = "/greeting")
-public Greeting greeting(){
-    greeting = new Greeting(1, "hello world");
+public Greeting greeting(@RequestParam("name") String name){
+    greeting = new Greeting(1, name);
     return greeting;
   }
 }
