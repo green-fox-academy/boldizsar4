2079cb403cd627a1bb8264e733a41b7aa9004a92
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-06-26 14:42:57 +0200

    feat(multiplehtml):mark the king

--- week-07/day-02/src/main/java/com/greenfoxacademy/bankofsimba/controllers/MultipleAccountController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/bankofsimba/controllers/MultipleAccountController.java
@@ -14,10 +14,11 @@ public class MultipleAccountController {
   List<BankAccount> characters = new ArrayList<>();
 
   public MultipleAccountController() {
-    characters.add(new BankAccount("Timon", 1200, "spider", "meerkat"));
-    characters.add(new BankAccount("Pumbaa", 4100, "rhizomes", "warthog"));
-    characters.add(new BankAccount("Mufasa", 6500, "zebra", "lion"));
-    characters.add(new BankAccount("Rafiki", 1300, "fruit", "mandrill"));
+    characters.add(new BankAccount("Simba", 8200, "zebra", "lion", true));
+    characters.add(new BankAccount("Timon", 1200, "spider", "meerkat", false));
+    characters.add(new BankAccount("Pumbaa", 4100, "rhizomes", "warthog", false));
+    characters.add(new BankAccount("Mufasa", 6500, "zebra", "lion", false));
+    characters.add(new BankAccount("Rafiki", 1300, "fruit", "mandrill",false));
   }
 
   @GetMapping("/charactertable")
--- week-07/day-02/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
@@ -6,14 +6,24 @@ public class BankAccount {
   private int balance;
   private String currency;
   private String animalType;
+  private boolean king;
 
 public BankAccount(){}
 
-  public BankAccount(String name, int balance, String currency, String animalType) {
+  public BankAccount(String name, int balance, String currency, String animalType, boolean king) {
     this.name = name;
     this.balance = balance;
     this.currency = currency;
     this.animalType = animalType;
+    this.king = king;
+  }
+
+  public boolean isKing() {
+    return king;
+  }
+
+  public void setKing(boolean king) {
+    this.king = king;
   }
 
   public String getName() {
