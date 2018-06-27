92d400cdd1b8634189dff4ef88c828bf14e444f2
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-06-27 13:51:21 +0200

    feat(coloredbackground):create random colored background done

--- week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/controllers/UsefulUtilitiesController.java
+++ week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/controllers/UsefulUtilitiesController.java
@@ -1,15 +1,25 @@
 package com.greenfoxacademy.webapplication.controllers;
 
+import com.greenfoxacademy.webapplication.services.UtilityService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsefulUtilitiesController {
-
+@Autowired
+  UtilityService utilityService;
   @GetMapping("/useful")
   public String link(Model model){
     model.addAttribute("utilities", "Colored Background");
     return "useful";
+  }
+
+  @GetMapping("useful/colored")
+  public String colored(Model model){
+    model.addAttribute("randomcolor", utilityService.randomColor());
+    return "colored";
   }
 }
