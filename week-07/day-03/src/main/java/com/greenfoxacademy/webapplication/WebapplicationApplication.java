0bc54445fd3ffe885b5d1627ceb533ffa99e2604
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-06-27 13:08:14 +0200

    hello bean world done

--- week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/WebapplicationApplication.java
+++ week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/WebapplicationApplication.java
@@ -1,12 +1,21 @@
 package com.greenfoxacademy.webapplication;
 
+import com.greenfoxacademy.webapplication.services.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebapplicationApplication {
-
+public class WebapplicationApplication implements CommandLineRunner {
+@Autowired
+Printer printer;
   public static void main(String[] args) {
     SpringApplication.run(WebapplicationApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
   }
 }
--- week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/services/HelloPrinter.java
+++
@@ -1,4 +0,0 @@
-package com.greenfoxacademy.webapplication.services;
-
-public class HelloPrinter {
-}
