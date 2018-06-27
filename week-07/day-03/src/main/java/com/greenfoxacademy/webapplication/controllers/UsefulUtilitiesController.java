d13d8bf7d8e76636b4bd39ae12859a8a10eecbe2
kiss.boldizsar44@gmail.com <38554645+boldizsar4@users.noreply.github.com>
2018-06-27 14:22:09 +0200

    feat(email):check valid emails done

--- week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/controllers/UsefulUtilitiesController.java
+++ week-07/day-03/src/main/java/com/greenfoxacademy/webapplication/controllers/UsefulUtilitiesController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UsefulUtilitiesController {
-@Autowired
+
+  @Autowired
   UtilityService utilityService;
+
   @GetMapping("/useful")
   public String link(Model model){
     model.addAttribute("utilities", "Colored Background");
@@ -21,5 +24,12 @@ public class UsefulUtilitiesController {
   public String colored(Model model){
     model.addAttribute("randomcolor", utilityService.randomColor());
     return "colored";
+  }
+
+  @GetMapping("useful/email")
+  public String email(Model model, @RequestParam(name = "email") String emailCheck){
+    model.addAttribute("isValid", utilityService.validateEmail(emailCheck));
+    model.addAttribute("emailToCheck", emailCheck);
+    return "email";
   }
 }
